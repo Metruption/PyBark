505d0c3859a2b4c1fc967228e0bb43e171df280d
Aaron Thomas <1d20m6@gmail.com>
2016-11-20 05:46:00 -0500

    Added GPL-3.0
    Because I love freedom.

--- src/pybark.py
+++ src/pybark.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+ PyBark: A wrapper for the Bark Partner API
+    Copyright (C) 2016  Aaron Thomas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 import requests
 
 def woof(BARK_TOKEN, message):
