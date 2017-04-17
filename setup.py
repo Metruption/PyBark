c693ae1981991ded2afcf425a076bda96aa31dc1
Aaron Thomas <ftk2ne@gmail.com>
2017-04-17 18:14:39 -0400

    Preparing for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'pyBark',
+  packages = ['pyBark'],
+  version = '1.0',
+  description = 'A python wrapper for the Bark Partners API',
+  author = 'Aaron Thomas',
+  author_email = '1d20m6@gmail.com',
+  url = 'https://github.com/Metruption/pyBark',
+  download_url = 'https://github.com/Metruption/pyBark/archive/1.0.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
+
