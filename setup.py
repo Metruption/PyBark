7aab73793100ec627d7258c4ec73a7d8d8ceb355
Aaron Thomas <ftk2ne@gmail.com>
2017-04-17 18:40:31 -0400

    Version 1.6

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyBark',
   packages = ['pyBark'],
-  version = '1.0',
+  version = '1.6',
   description = 'A python wrapper for the Bark Partners API',
   author = 'Aaron Thomas',
   author_email = '1d20m6@gmail.com',
   url = 'https://github.com/Metruption/pyBark',
-  download_url = 'https://github.com/Metruption/pyBark/archive/v1.5.tar.gz',
+  download_url = 'https://github.com/Metruption/pyBark/archive/v1.6.tar.gz',
   keywords = [],
   classifiers = [],
 )
